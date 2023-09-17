4cc542cc024ff3e24428f6e505a857703b0b2681
Chefback <chefbacksw@gmail.com>
2023-09-17 17:33:44 +0800

    refactor(sidebar-components): encap sidebtn

---
+++ src/views/SidePanel/components/SideBtn.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <n-button :style="{ width: '100%' }">{{ btname }}</n-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { NButton } from 'naive-ui';
+defineProps<{ btname: string }>()
+</script>
+
+<style scoped>
+div {
+
+    /* .btnstyle {
+
+        width: 15vw;
+    } */
+}
+</style>
