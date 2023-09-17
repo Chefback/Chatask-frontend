8d4624937b29ddfda460accc47e4ebacfba40be0
Chefback <chefbacksw@gmail.com>
2023-09-17 23:41:41 +0800

    feat(views/TodoList): put Header and Item together

--- src/views/TodoList/TodoList.vue
+++ src/views/TodoList/TodoList.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-这是清单面板，显示清单的列表
+    <div class="container">
+
+        <list-header></list-header>
+        <div v-for="(item) in Array(10)" class="list-style">
+            <list-item></list-item>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import ListItem from "./components/ListItem.vue";
+import ListHeader from "./components/ListHeader.vue";
+
 
 </script>
 
 <style scoped>
-
+.list-style {
+    display: flex;
+    flex-direction: column;
+}
 </style>
